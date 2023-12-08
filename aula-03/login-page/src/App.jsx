7f2d807514f18d9c4bb0895f1ec9af965222b419
Vinicios Neves <vinicios_neves@hotmail.com>
2023-12-08 13:23:58 +0000

    aula 3 - parte 1

---
+++ aula-03/login-page/src/App.jsx
@@ -0,0 +1,16 @@
+import GlobalStyle from './componentes/EstilosGlobais/EstilosGlobais'
+import SubTitulo from './componentes/SubTitulo/SubTitulo'
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      <SubTitulo>
+        Nós Transformamos Ideias em Inovação
+      </SubTitulo>
+    </>
+  )
+}
+
+export default App
---
+++ aula-03/login-page/src/componentes/SubTitulo/SubTitulo.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const SubTitulo = styled.h2`
+    color: #B0B0B0;
+    font-family: 'GothamLight';
+    font-size: 25px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 50px;
+    letter-spacing: 4.25px;
+`
+
+export default SubTitulo
