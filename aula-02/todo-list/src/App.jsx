0886c0d7044c1905733f512d899b5bbe285ba922
Vinicios Neves <vinicios_neves@hotmail.com>
2023-12-08 11:29:23 +0000

    aula 2 - parte 3 - useState

--- aula-02/todo-list/src/App.jsx
+++ aula-02/todo-list/src/App.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Titulo from './componentes/Titulo/Titulo';
 import TodoItem from './componentes/TodoItem/TodoItem';
 
 function App() {
-  const tarefas = ["Aprender React", "Revisar JSX", "Construir uma App"];
+  const [tarefas, setTarefas] = useState(["Aprender React", "Revisar JSX", "Construir uma App"]);
+
+  const [novaTarefa, setNovatarefa] = useState('')
+
+  function aoSubmeterFormulario (evento) {
+    evento.preventDefault()
+    setTarefas([...tarefas, novaTarefa])
+    setNovatarefa('')
+  }
 
   return (<div className="container">
 
       <Titulo>Tarefas</Titulo>
+
+      <form onSubmit={aoSubmeterFormulario}>
+        <input 
+          type="text" 
+          className='input-texto' 
+          placeholder='Digite e pressione enter para cadastrar'
+          value={novaTarefa}
+          onChange={evento => setNovatarefa(evento.target.value)}
+        />
+      </form>
+
 
       {tarefas.map((tarefa, index) => (
           <TodoItem key={index} descricao={tarefa} />
